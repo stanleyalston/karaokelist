4610c17e550262e4efcbcbc61e7466ac48d59fbf
Stanley Alston <stanleyalston@hotmail.com>
2013-05-17 22:50:03 +0800

    Added pages and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Karaokelist::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/new"
+
+  get "pages/list"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
